e7b52b5b7ac4416736534857ddb967246d6a94e1
Sharon RH <sharon.rhe@gmail.com>
2015-01-12 00:14:20 +0700

    integrate addgame back & front. causing error on image_crud somehow..

--- CI/application/views/game/addGame.php
+++ CI/application/views/game/addGame.php
@@ -1,6 +1,12 @@
 <html>
 	<head>
-		<title>Add New Games</title>
+		<title>Game - Dashboard</title>
+		<meta charset="UTF-8">
+         <meta name="viewport" content="width=device-width">
+        <!-- Bootstrap -->
+        <link href=<?php echo base_url("assets/css/style-dashboard.css");?> rel="stylesheet" media="screen">
+        <script src=<?php echo base_url("js/jquery-latest.min.js");?> type="text/javascript"></script>
+        <script type="text/javascript" src=<?php echo base_url("js/bootstrap.js");?>></script>
 		<script type="text/javascript" src=<?php echo base_url("application/third_party/tinymce/js/tinymce/tinymce.min.js");?>></script>
 		<script type="text/javascript">
 		tinymce.init({
@@ -24,6 +30,56 @@
 	</head>
 
 	<body>
+		<div id="container">
+            <header>
+        		<?php $this->load->view('admin/header-dashboard'); ?>
+            </header>
+            <div id="main">
+                <div id="jumlah-donasi">
+                    <div class="row">
+                        <div class="col-md-7">
+                            <div id="add">
+                                <h1>Add Showcase</h1>
+                                <?php echo form_open('game/addNew', array('role' => 'form')); ?>
+                                  <div class="form-group">
+                                    <label for="title">Judul</label>
+                                    <?php echo form_input(array('name' => 'title', 'class' => 'form-control', 'placeholder' => 'Enter Title'));?>
+                                  </div>
+                                  <div class="form-group">
+                                    <label for="desc">Showcase Content</label>
+                                    <?php echo form_textarea(array('name' => 'desc', 'class' => 'form-control', 'rows' => '5'));?>
+                                  </div>
+                                  <div class="form-group">
+                                    <label for="thumbnail">Thumbnail</label>
+                                    <?php echo form_upload('thumbnail'); ?>
+                                    <p class="help-block">Silakan pilih gambar untuk thumbnail showcase</p>
+                                  </div>
+                                  <div class="form-group">
+                                    <label for="gambarBerita">Gambar Utama Showcase</label>
+                                    <?php echo form_upload('gambarBerita'); ?>
+                                    <p class="help-block">Silakan pilih gambar utama showcase</p>
+                                  </div>
+                                  <div class="form-group">
+<!--                                     <label class="col-md-3" for="screenshot1">Screenshot 1:</label>
+                                    <input class="col-md-9" type="file" id="screenshot1">
+                                    <label class="col-md-3" for="screenshot2">Screenshot 2:</label>
+                                    <input class="col-md-9" type="file" id="screenshot2">
+                                    <label class="col-md-3" for="screenshot3">Screenshot 3:</label>
+                                    <input class="col-md-9" type="file" id="screenshot3">
+                                    <label class="col-md-3" for="screenshot4">Screenshot 4:</label>
+                                    <input class="col-md-9" type="file" id="screenshot4">
+ -->                                  </div>
+                                  <?php echo form_submit('submit', 'Save','id="submit"'); ?>
+                            	<?php echo form_close();?>
+                            	<?php echo $output; ?>
+                            </div>
+	                    </div>
+                    </div>
+                </div>
+            </div>
+            <footer></footer>
+        </div>
+
 <!--
 		<script type ="text/javacript" src="<?=base_url()?>js/jquery-1.11.1.js>"</script>
 		<script type ="text/javacript" src="<?=base_url()?>js/jquery-ui.min.js>"</script>
@@ -33,36 +89,6 @@
 		<script type="text/javascript" src="<?=base_url()?>js/plupload/js/jquery.plupload.queue/jquery.plupload.queue.js>"</script>
 
 		<div id="uploadBox" title="UploadBox"></div> -->
-		<?php echo form_open('game/addNew'); ?>
-		<table>
-			<tr>
-				<td>Title</td>
-				<td>:</td>
-				<td><?php echo form_input('title');?></td>
-			</tr>
-			<tr>
-				<td>Description</td>
-				<td>:</td>
-				<td><?php echo form_textarea('desc');?></td>
-			</tr>
-			<tr>
-				<td>Released At</td>
-				<td>:</td>
-				<td><?php echo form_input('date');?></td>
-			</tr>
-			<tr>
-				<td>Available On</td>
-				<td>:</td>
-				<td><?php echo form_input('avail');?></td>
-			</tr>
-			<tr>
-				<td><?php echo form_submit('submit', 'Save','id="submit"') ?></td>
-			</tr>
-		</table>
-		<?php echo form_close();?>
-		<?php echo $output; ?>
-		</br>
-		</br>
 
 		<a href="http://localhost/CI/index.php/login/logout">Logout</a>
 	</body>
