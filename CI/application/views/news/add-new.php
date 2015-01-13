f186df934be8dbfa9c644896640b29b89c536936
Sharon RH <sharon.rhe@gmail.com>
2015-01-13 13:10:39 +0700

    move new views to proper directories

--- CI/application/views/news/add-new.php
+++ CI/application/views/news/add-new.php
@@ -10,21 +10,24 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width">
         <!-- Bootstrap -->
-        <link href=<?php echo base_url("assets/css/style-dashboard.css");?> rel="stylesheet" media="screen">
-        <script src=<?php echo base_url("js/jquery-latest.min.js");?> type="text/javascript"></script>
-        <script type="text/javascript" src=<?php echo base_url("js/bootstrap.js");?>></script>
+        <link href="css/style-dashboard.css" rel="stylesheet" media="screen">
+        <script src="js/jquery-latest.min.js" type="text/javascript"></script>
+        <script type="text/javascript" src="js/bootstrap.js"></script>
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
         <!--[if lt IE 9]>
           <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
           <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
         <![endif]-->
+        <script>
+            $(function() {
+                $("header").load("header-dashboard.html");
+            });
+        </script>
     </head>
     <body>
         <div id="container">
-            <header>
-                <?php $this->load->view('header-dashboard'); ?>
-            </header>
+            <header></header>
             <div id="main">
                 <div id="jumlah-donasi">
                     <div class="row">
