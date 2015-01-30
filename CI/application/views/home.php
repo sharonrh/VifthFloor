665ee6572ac7474a61b67e7a63e085398fbb63c2
bonitabone <ramadhanibonita@yahoo.com>
2015-01-30 23:08:35 +0700

    benerin tinggi footer

--- CI/application/views/home.php
+++ CI/application/views/home.php
@@ -21,22 +21,21 @@ and open the template in the editor.
           <![endif]-->
       </head>
       <body>
-        <div id="container">
+        <div id="container" class="home-container">
             <header id="header-home">
                 <?php $this->load->view('header'); ?> 
             </header>
-            <div>
-                <div id="headlineCarousel" class="carousel slide" data-ride="carousel">
-                    <!-- Indicators -->
-                    <ol class="carousel-indicators">
-                        <li data-target="#headlineCarousel" data-slide-to="0" class="active"></li>
-                        <li data-target="#headlineCarousel" data-slide-to="1"></li>
-                        <li data-target="#headlineCarousel" data-slide-to="2"></li>
-                    </ol>
+            <div id="headlineCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#headlineCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#headlineCarousel" data-slide-to="1"></li>
+                    <li data-target="#headlineCarousel" data-slide-to="2"></li>
+                </ol>
 
-                    <!-- Wrapper for slides -->
+                <!-- Wrapper for slides -->
 
-                    <div class="carousel-inner">
+                <div class="carousel-inner">
                        <!-- <?php $count=0; ?>
                             <?php foreach ($slides as $row): ?>
 
@@ -58,65 +57,64 @@ and open the template in the editor.
                                 <?php $count++; ?>
 
                             <?php endforeach; ?>  -->
-                        <div class="item active">
-                            <img class="img-responsive" height ="100px" src="<?php echo base_url();?>/assets/images/satu.jpg" alt="">
-                            <div class="carousel-caption">
-                            </div>
-                        </div>
-                        <div class="item">
-                            <img class="img-responsive" height ="100px" src="<?php echo base_url();?>/assets/images/dua.jpg" alt="">
-                            <div class="carousel-caption">
-                            </div>
-                        </div>
-                        <div class="item">
-                            <img class="img-responsive" height ="100px" src="<?php echo base_url();?>/assets/images/tiga.jpg" alt="">
-                            <div class="carousel-caption">
-                            </div>
-                        </div>          
-                        
-                    </div>
-                </div>
-
-                <div id="latest-news">
-                    <div class="row">
-
-                        <div class="col-sm-11">
-                            <div class="row">
-                                <?php foreach ($news as $row): ?>
-                                <div class="col-sm-3 padding-left-15 padding-right-5">
-                                    <figure class="cap-top">
-                                        <a href="<?php echo site_url();?>/news/view/<?php echo $row->Id;?>" class="thumbnail">
-                                            <img src="<?php echo base_url();?>/assets/uploads/newsThumbnail/<?php echo $row->Thumbnail;?>"/>
-                                            <div class="slide-caption">
-                                                <p class="isi-caption"><?php echo $row->Title;?></p>
-                                            </div>
-                                        </a>
-                                    </figure>
+                            <div class="item active">
+                                <img class="img-responsive" height ="100px" src="<?php echo base_url();?>/assets/images/satu.jpg" alt="">
+                                <div class="carousel-caption">
                                 </div>
-                                <?php endforeach; ?>
-                                
                             </div>
+                            <div class="item">
+                                <img class="img-responsive" height ="100px" src="<?php echo base_url();?>/assets/images/dua.jpg" alt="">
+                                <div class="carousel-caption">
+                                </div>
+                            </div>
+                            <div class="item">
+                                <img class="img-responsive" height ="100px" src="<?php echo base_url();?>/assets/images/tiga.jpg" alt="">
+                                <div class="carousel-caption">
+                                </div>
+                            </div>          
+                            
                         </div>
-                        
-                        <div class="col-sm-1">
-                            <div class="row">
-                                <div class="col-sm-12">
-                                    <div class="box-information-index">
-                                        <div class="view-more">
-                                            <a href=<?php echo site_url("news");?>>More News</a>
+                    </div>
+
+                    <div id="latest-news">
+                        <div class="row">
+
+                            <div class="col-sm-11">
+                                <div class="row">
+                                    <?php foreach ($news as $row): ?>
+                                        <div class="col-sm-3 padding-left-15 padding-right-5">
+                                            <figure class="cap-top">
+                                                <a href="<?php echo site_url();?>/news/view/<?php echo $row->Id;?>" class="thumbnail">
+                                                    <img src="<?php echo base_url();?>/assets/uploads/newsThumbnail/<?php echo $row->Thumbnail;?>"/>
+                                                    <div class="slide-caption">
+                                                        <p class="isi-caption"><?php echo $row->Title;?></p>
+                                                    </div>
+                                                </a>
+                                            </figure>
+                                        </div>
+                                    <?php endforeach; ?>
+                                    
+                                </div>
+                            </div>
+                            
+                            <div class="col-sm-1">
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <div class="box-information-index">
+                                            <div class="view-more">
+                                                <a href=<?php echo site_url("news");?>>More News</a>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
+
+
                         </div>
-
-
                     </div>
+                    <footer>
+                        <?php $this->load->view('footer'); ?>
+                    </footer>
                 </div>
-            </div>
-            <footer>
-                <?php $this->load->view('footer'); ?>
-            </footer>
-        </div>
-    </body>
-    </html>
+            </body>
+            </html>
